551719d986c4ce5bb08168e3eef433d3600a0292
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-07-11 13:16:06 +0300

    Add example for wallet-connect.

---
+++ signing-providers/src/wallet-connect.js
@@ -0,0 +1,83 @@
+import QRCodeModal from "@walletconnect/qrcode-modal";
+import { WalletConnectProvider } from "@elrondnetwork/erdjs-wallet-connect-provider";
+import { Address, Transaction, TransactionPayload } from "@elrondnetwork/erdjs"
+
+const bridgeUrl = "https://bridge.walletconnect.org";
+
+export class WalletConnect {
+    constructor() {
+        this.provider = new WalletConnectProvider(bridgeUrl, this.prepareCallbacks());
+    }
+
+    prepareCallbacks() {
+        const self = this;
+
+        return {
+            onClientLogin: async function () {
+                QRCodeModal.close();
+                const address = await self.provider.getAddress();
+                alert(`onClientLogin(), address: ${address}`);
+            },
+            onClientLogout: async function () {
+                alert("onClientLogout()");
+            }
+        };
+    }
+
+    async login() {
+        await this.provider.init();
+        const connectorUri = await this.provider.login();
+        QRCodeModal.open(connectorUri);
+    }
+
+    async logout() {
+        await this.provider.init();
+        await this.provider.logout();
+    }
+
+    async signTransaction() {
+        const transaction = new Transaction({
+            nonce: 42,
+            value: "1",
+            receiver: new Address("erd1uv40ahysflse896x4ktnh6ecx43u7cmy9wnxnvcyp7deg299a4sq6vaywa"),
+            gasPrice: 1000000000,
+            gasLimit: 50000,
+            data: new TransactionPayload(),
+            chainID: "T",
+            version: 1
+        });
+
+        await this.provider.signTransaction(transaction);
+
+        alert(JSON.stringify(transaction.toSendable(), null, 4));
+    }
+
+    async signTransactions() {
+        const firstTransaction = new Transaction({
+            nonce: 43,
+            value: "1",
+            receiver: new Address("erd1uv40ahysflse896x4ktnh6ecx43u7cmy9wnxnvcyp7deg299a4sq6vaywa"),
+            gasPrice: 1000000000,
+            gasLimit: 50000,
+            data: new TransactionPayload(),
+            chainID: "T",
+            version: 1
+        });
+
+        const secondTransaction = new Transaction({
+            nonce: 44,
+            value: "100000000",
+            receiver: new Address("erd1uv40ahysflse896x4ktnh6ecx43u7cmy9wnxnvcyp7deg299a4sq6vaywa"),
+            gasPrice: 1000000000,
+            gasLimit: 50000,
+            data: new TransactionPayload("hello world"),
+            chainID: "T",
+            version: 1
+        });
+
+        const transactions = [firstTransaction, secondTransaction];
+        await this.provider.signTransactions(transactions);
+
+        alert(JSON.stringify([firstTransaction.toSendable(), secondTransaction.toSendable()], null, 4));
+    }
+}
