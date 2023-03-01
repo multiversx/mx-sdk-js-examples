feb0d3a723abcbe9b54387770ad7d899b2e5b5fe
Radu Mojic <radu.mojic@elrond.com>
2023-03-01 10:37:36 +0200

    [+] updated the package version

--- signing-providers/src/wallet-connect-v2.js
+++ signing-providers/src/wallet-connect-v2.js
@@ -2,10 +2,9 @@ import QRCode from "qrcode";
 import { WalletConnectV2Provider } from "@multiversx/sdk-wallet-connect-provider";
 import { Address, SignableMessage, Transaction, TransactionPayload } from "@elrondnetwork/erdjs";
 
-const relayUrl = "wss://relay.walletconnect.com";
+// Generate your own WalletConnect 2 ProjectId here: https://cloud.walletconnect.com/app
 const projectId = "9b1a9564f91cb659ffe21b73d5c4e2d8";
-
-
+const relayUrl = "wss://relay.walletconnect.com";
 
 export class WalletConnectV2 {
     constructor() {
