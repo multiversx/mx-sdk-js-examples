f541466a6dbbb53ffb58a101f1efd64084955753
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-02-27 19:01:18 +0200

    Broadcasting transactions, await for completion.

--- cookbook/samples.js
+++ cookbook/samples.js
@@ -3,6 +3,11 @@ import { Address, Transaction, TransactionNext, TransactionPayload } from "@mult
 export const addressOfAlice = new Address("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th");
 export const addressOfBob = new Address("erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx");
 export const addressOfCarol = new Address("erd1k2s324ww2g0yj38qn2ch2jwctdy8mnfxep94q9arncc6xecg3xaq6mjse8");
+export const completedTransactionsHashes = [
+    "930e9e1a687602522f9b0803eae8d5f0a07ac6721ef5bb455a989e38b2681e5f",
+    "9a4cce1b5e1c6edbd06aa509b09e02596b42afbdfa3f43db1245fd4634fc6815",
+    "7d7d05f141026aa461154b52476a5a78bd3dd9b0dc29bedc66b0de54c55a41d5"
+];
 
 export const legacyDelegationContractAddress = Address.fromBech32("erd1qqqqqqqqqqqqqpgqp699jngundfqw07d8jzkepucvpzush6k3wvqyc44rx");
 export const addressOfFirstDevnetDelegator = new Address("erd1s0us936aku52uxyvnhxspcaj4f4sp7d9azuyw7kf32ggm88ynlps7c0yr9");
