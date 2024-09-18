594465208fd3a9d5c57bca8ecc94f2dc59cbf4a6
Tudor Morar <tudor.morar@multiversx.com>
2024-09-18 15:36:59 +0300

    Make guard ledger transactions

---
+++ signing-providers/src/config.devnet.js
@@ -0,0 +1,11 @@
+import { WALLET_PROVIDER_DEVNET } from "@multiversx/sdk-web-wallet-provider";
+
+export const CHAIN_ID = "D";
+export const NETWORK_NAME = "Devnet";
+export const XALIAS_URL = "https://devnet.xalias.com";
+export const WALLET_PROVIDER_URL = WALLET_PROVIDER_DEVNET;
+export const API_URL = "https://devnet-api.multiversx.com";
+
+// Generate your own WalletConnect 2 ProjectId here: https://cloud.walletconnect.com/app
+export const WALLET_CONNECT_PROJECT_ID = "9b1a9564f91cb659ffe21b73d5c4e2d8";
+export const WALLET_CONNECT_RELAY_URL = "wss://relay.walletconnect.com";
