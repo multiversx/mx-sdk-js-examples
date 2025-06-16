e30248bc96134fa7e25c919300dd63a5c2b0024e
Tudor Morar <tudor.morar@multiversx.com>
2025-06-16 17:54:49 +0300

    Initial commit: provider working

---
+++ signing-providers/src/iframe-communication.js
@@ -0,0 +1,45 @@
+import { IframeProvider } from "@multiversx/sdk-web-wallet-iframe-provider/out";
+import { IframeLoginTypes } from "@multiversx/sdk-web-wallet-iframe-provider/out/constants";
+
+// IMPORTANT: The iframe wallet must be served over HTTPS on different domain
+// example: http-server -c-1 -S -C ./dummy-certificate.pem -K ./dummy-certificate-key.pem --port=3000
+const IFRAME_WALLET_URL = `https://192.168.50.183:3000/iframeWallet.html`;
+
+export class IframeCommunication {
+  constructor() {
+    this._provider = IframeProvider.getInstance();
+  }
+
+  async init() {
+    this._provider.setLoginType(IframeLoginTypes.metamask);
+    this._provider.setWalletUrl(IFRAME_WALLET_URL);
+    const isInitialized = await this._provider.init();
+    return isInitialized;
+  }
+
+  async login() {
+    await this.init();
+    const account = await this._provider.login();
+    alert(`Address: ${account.address}`);
+  }
+
+  async logout() {
+    throw new Error("Not implemented");
+  }
+
+  async signTransaction() {
+    throw new Error("Not implemented");
+  }
+
+  async loginWithToken() {
+    throw new Error("Not implemented");
+  }
+
+  async logout() {
+    throw new Error("Not implemented");
+  }
+
+  async signMessage() {
+    throw new Error("Not implemented");
+  }
+}
--- signing-providers/src/webview.js
+++ signing-providers/src/webview.js
@@ -1,125 +1,137 @@
-import { Address, Message, Transaction, TransactionPayload } from "@multiversx/sdk-core";
+import {
+  Address,
+  Message,
+  Transaction,
+  TransactionPayload,
+} from "@multiversx/sdk-core";
 import { CHAIN_ID } from "./config";
 import { WebviewProvider } from "@multiversx/sdk-webview-provider/out/WebviewProvider";
 import { displayOutcome } from "./helpers";
 
-export const addressOfAlice = new Address("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th");
+export const addressOfAlice = new Address(
+  "erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"
+);
 
 export class Webview {
-    constructor() {
-        this._provider = WebviewProvider.getInstance({
-            resetStateCallback: () => console.log("Reset state callback called"),
-        });
+  constructor() {
+    this._provider = WebviewProvider.getInstance({
+      resetStateCallback: () => console.log("Reset state callback called"),
+    });
+  }
+
+  async init() {
+    return await this._provider.init();
+  }
+
+  async login() {
+    const response = await this._provider.login();
+
+    console.log("Login response:" + JSON.stringify(response));
+    alert("Login response:" + JSON.stringify(response));
+
+    return response;
+  }
+
+  async logout() {
+    const response = await this._provider.logout();
+
+    console.log("Logout response:" + JSON.stringify(response));
+    alert("Logout response:" + JSON.stringify(response));
+
+    return response;
+  }
+
+  async relogin() {
+    const accessToken = await this._provider.relogin();
+
+    alert("accessToken = " + JSON.stringify(accessToken));
+    console.log("accessToken = " + JSON.stringify(accessToken));
+
+    if (!accessToken) {
+      console.error("Unable to re-login. Missing accessToken.");
+      alert("Unable to re-login. Missing accessToken.");
+      return null;
     }
 
-    async init(){
-        return await this._provider.init();
-    };
+    return accessToken;
+  }
 
-    async login() {
-        const response = await this._provider.login();
+  async signTransaction() {
+    const transaction = new Transaction({
+      nonce: 42,
+      value: "1",
+      sender: addressOfAlice,
+      receiver: new Address(
+        "erd1uv40ahysflse896x4ktnh6ecx43u7cmy9wnxnvcyp7deg299a4sq6vaywa"
+      ),
+      gasPrice: 1000000000,
+      gasLimit: 50000,
+      data: new TransactionPayload(),
+      chainID: CHAIN_ID,
+      version: 1,
+    });
 
-        console.log("Login response:" + JSON.stringify(response));
-        alert("Login response:" + JSON.stringify(response));
+    const response = await this._provider.signTransaction(transaction);
 
-        return response;
+    console.log("Sign transaction response:" + JSON.stringify(response));
+    alert("Sign transaction response:" + JSON.stringify(response));
+
+    return response;
+  }
+
+  async signTransactions() {
+    const transaction = new Transaction({
+      nonce: 42,
+      value: "1",
+      sender: addressOfAlice,
+      receiver: new Address(
+        "erd1uv40ahysflse896x4ktnh6ecx43u7cmy9wnxnvcyp7deg299a4sq6vaywa"
+      ),
+      gasPrice: 1000000000,
+      gasLimit: 50000,
+      data: new TransactionPayload(),
+      chainID: CHAIN_ID,
+      version: 1,
+    });
+
+    const response = await this._provider.signTransactions([transaction]);
+
+    if (!response) {
+      this._provider.cancelAction();
+      return null;
     }
 
-    async logout(){
-        const response = await this._provider.logout();
+    console.log("Sign transactions response:" + JSON.stringify(response));
+    alert("Sign transactions response:" + JSON.stringify(response));
 
-        console.log("Logout response:" + JSON.stringify(response));
-        alert("Logout response:" + JSON.stringify(response));
+    return response;
+  }
 
-        return response;
-    };
+  async signMessage() {
+    const message = new Message({
+      address: new Address(
+        "erd1uv40ahysflse896x4ktnh6ecx43u7cmy9wnxnvcyp7deg299a4sq6vaywa"
+      ),
+      data: Buffer.from("hello"),
+    });
 
-    async relogin() {
-        const accessToken = await this._provider.relogin();
+    const signedMessage = await this._provider.signMessage(message);
 
-        alert("accessToken = " + JSON.stringify(accessToken));
-        console.log("accessToken = " + JSON.stringify(accessToken));
+    displayOutcome(
+      "Message signed. Signature: ",
+      Buffer.from(signedMessage?.signature).toString("hex")
+    );
+  }
 
-        if (!accessToken) {
-            console.error('Unable to re-login. Missing accessToken.');
-            alert('Unable to re-login. Missing accessToken.')
-            return null;
-        }
+  async cancelAction() {
+    return await this._provider.cancelAction();
+  }
 
-        return accessToken;
-    };
+  async isInitialized() {
+    return this._provider.isInitialized();
+  }
 
-    async signTransaction() {
-        const transaction = new Transaction({
-            nonce: 42,
-            value: "1",
-            sender: addressOfAlice,
-            receiver: new Address("erd1uv40ahysflse896x4ktnh6ecx43u7cmy9wnxnvcyp7deg299a4sq6vaywa"),
-            gasPrice: 1000000000,
-            gasLimit: 50000,
-            data: new TransactionPayload(),
-            chainID: CHAIN_ID,
-            version: 1
-        });
-
-        const response = await this._provider.signTransaction(transaction);
-
-        console.log("Sign transaction response:" + JSON.stringify(response));
-        alert("Sign transaction response:" + JSON.stringify(response));
-
-        return response;
-    }
-
-    async signTransactions() {
-        const transaction = new Transaction({
-            nonce: 42,
-            value: "1",
-            sender: addressOfAlice,
-            receiver: new Address("erd1uv40ahysflse896x4ktnh6ecx43u7cmy9wnxnvcyp7deg299a4sq6vaywa"),
-            gasPrice: 1000000000,
-            gasLimit: 50000,
-            data: new TransactionPayload(),
-            chainID: CHAIN_ID,
-            version: 1
-        });
-
-        const response = await this._provider.signTransactions([transaction]);
-
-        if (!response) {
-            this._provider.cancelAction();
-            return null;
-        }
-
-        console.log("Sign transactions response:" + JSON.stringify(response));
-        alert("Sign transactions response:" + JSON.stringify(response));
-
-        return response;
-    }
-
-    async signMessage() {
-        const message = new Message({
-            address: new Address("erd1uv40ahysflse896x4ktnh6ecx43u7cmy9wnxnvcyp7deg299a4sq6vaywa"),
-            data: Buffer.from("hello"),
-        });
-        
-        const signedMessage = await this._provider.signMessage(message);
-
-        displayOutcome(
-            "Message signed. Signature: ",
-            Buffer.from(signedMessage?.signature).toString("hex")
-        );
-    }
-
-    async cancelAction() {
-        return await this._provider.cancelAction();
-    };
-
-    async isInitialized () {
-        return this._provider.isInitialized();
-    };
-
-    async isConnected(){
-        return await this._provider.isConnected();
-    };
+  async isConnected() {
+    return await this._provider.isConnected();
+  }
 }
-
