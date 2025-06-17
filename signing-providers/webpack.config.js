0bef1df48ac338a47c617efced015418151d4f00
Tudor Morar <tudor.morar@multiversx.com>
2025-06-17 15:50:45 +0300

    Lint webpack

--- signing-providers/webpack.config.js
+++ signing-providers/webpack.config.js
@@ -10,47 +10,47 @@ const webpack = require("webpack");
 
 /** @type WebpackConfig */
 const config = {
-  mode: "none",
-  entry: {
-    app: "./src/index.js",
-  },
-  output: {
-    filename: "[name].js",
-    path: path.join(__dirname, "./out"),
-    libraryTarget: "umd",
-  },
-  resolve: {
-    mainFields: ["browser", "module", "main"],
-    extensions: [".ts", ".js"], // support ts-files and js-files
-    alias: {
-      // provides alternate implementation for node module and source files
+    mode: "none",
+    entry: {
+        app: "./src/index.js",
     },
-    fallback: {
-      // Webpack 5 no longer polyfills Node.js core modules automatically.
-      // see https://webpack.js.org/configuration/resolve/#resolvefallback
-      // for the list of Node.js core module polyfills.
-      assert: require.resolve("assert"),
-      buffer: require.resolve("buffer"),
-      stream: require.resolve("stream-browserify"),
-      crypto: require.resolve("crypto-browserify"),
-      path: require.resolve("path-browserify"),
-      punycode: require.resolve("punycode"),
-      fs: false,
+    output: {
+        filename: "[name].js",
+        path: path.join(__dirname, "./out"),
+        libraryTarget: "umd",
     },
-  },
-  module: {
-    rules: [],
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ["buffer", "Buffer"],
-    }),
-  ],
-  externals: {},
-  performance: {
-    hints: false,
-  },
-  devtool: "eval-source-map", // create a source map that points to the original source file
+    resolve: {
+        mainFields: ["browser", "module", "main"],
+        extensions: [".ts", ".js"], // support ts-files and js-files
+        alias: {
+            // provides alternate implementation for node module and source files
+        },
+        fallback: {
+            // Webpack 5 no longer polyfills Node.js core modules automatically.
+            // see https://webpack.js.org/configuration/resolve/#resolvefallback
+            // for the list of Node.js core module polyfills.
+            assert: require.resolve("assert"),
+            buffer: require.resolve("buffer"),
+            stream: require.resolve("stream-browserify"),
+            crypto: require.resolve("crypto-browserify"),
+            path: require.resolve("path-browserify"),
+            punycode: require.resolve("punycode"),
+            fs: false,
+        },
+    },
+    module: {
+        rules: [],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            Buffer: ["buffer", "Buffer"],
+        }),
+    ],
+    externals: {},
+    performance: {
+        hints: false,
+    },
+    devtool: "eval-source-map", // create a source map that points to the original source file
 };
 
 module.exports = [config];
