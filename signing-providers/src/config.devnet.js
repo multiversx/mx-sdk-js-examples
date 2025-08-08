7a78b9c7831705445dc0dc5cfcfef6a0adf9f601
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-08-08 17:37:52 +0300

    Update ledger

--- signing-providers/src/config.devnet.js
+++ signing-providers/src/config.devnet.js
@@ -4,6 +4,7 @@ export const CHAIN_ID = "D";
 export const NETWORK_NAME = "Devnet";
 export const WALLET_PROVIDER_URL = WALLET_PROVIDER_DEVNET;
 export const API_URL = "https://devnet-api.multiversx.com";
+export const PROXY_URL = "https://devnet-gateway.multiversx.com";
 
 // Generate your own WalletConnect 2 ProjectId here: https://cloud.walletconnect.com/app
 export const WALLET_CONNECT_PROJECT_ID = "9b1a9564f91cb659ffe21b73d5c4e2d8";
